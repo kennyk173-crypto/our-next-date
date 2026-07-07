513bead77ea5bc086564f3a243f78f1b5797cc00
Kenneth Kady <kennethkady@KennethdeMacBook-Pro.local>
2026-07-07 17:55:41 +0800

    slightly adjusted wallpaper size to remove black bars

--- scripts/crop_stickers.py
+++ scripts/crop_stickers.py
@@ -1,8 +1,7 @@
 from PIL import Image
 
-image = Image.open("public/pictures/stickers.PNG")
+image = Image.open("scripts/stickers.PNG")
 print(image.size)
 
-cropped = image.crop((0, 250, 1170, 2250))
-
-cropped.save("cropped.png")
+cropped = image.crop((0, 270, 1170, 2250))
+cropped.save("scripts/cropped.png")
